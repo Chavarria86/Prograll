d16833e6a500c9ec2c853dce9272060f1f1dfbcc
chavarria86 <USSS027724@ugb.edu.sv>
2025-02-25 07:03:38 -0600

    calculadoras completa

--- app/src/main/java/com/ugb/mIPrimerAplicacion/MainActivity.java
+++ app/src/main/java/com/ugb/mIPrimerAplicacion/MainActivity.java
@@ -2,6 +2,7 @@ package com.ugb.mIPrimerAplicacion;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -18,113 +19,93 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    TabHost tbh;
     Button btn;
     TextView tempVal;
-    EditText textCantidad;
-    TextView lblRespuesta;
-    Spinner spnDe, spnA;
-    conversores objConversores = new conversores();
+    RadioGroup rgb;
+    RadioButton opt;
+    EditText num1, num2;
 
+    Spinner spn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tbh = findViewById(R.id.tbhConversor);
-        tbh.setup();
-        tbh.addTab(tbh.newTabSpec("Moneda").setContent(R.id.tabMoneda).setIndicator("MONEDAS", null));
-        tbh.addTab(tbh.newTabSpec("Longitud").setContent(R.id.tabLongitud).setIndicator("LONGITUD", null));
-        tbh.addTab(tbh.newTabSpec("Tiempo").setContent(R.id.tabTiempo).setIndicator("TIEMPO", null));
-        tbh.addTab(tbh.newTabSpec("Almacenamiento").setContent(R.id.tabAlmacenamiento).setIndicator("ALMACENAMIENTO", null));
-        tbh.addTab(tbh.newTabSpec("Transferencia").setContent(R.id.tabTransferencia).setIndicator("TRANSFERENCIA", null)); //
-        tbh.addTab(tbh.newTabSpec("Masa").setContent(R.id.tabMasa).setIndicator("MASA", null)); //
-        tbh.addTab(tbh.newTabSpec("Volumen").setContent(R.id.tabVolumen).setIndicator("VOLUMEN", null)); //
         btn = findViewById(R.id.btnCalcular);
-        textCantidad = findViewById(R.id.textCantidad);
-        lblRespuesta = findViewById(R.id.lblRespuesta);
+        num1 = findViewById(R.id.txtNum1);
+        num2 = findViewById(R.id.txtNum2);
+
+        spn = findViewById(R.id.spnOpciones);
+        spn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 6 || position == 7 || position == 8) {
+                    num2.setText("0.00");
+                    num2.setEnabled(false);
+                } else {
+                    num2.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                try {
-                    int opcion = tbh.getCurrentTab();
+            public void onClick(View v) {
+                tempVal = findViewById(R.id.txtNum1);
+                double num1 = Double.parseDouble(tempVal.getText().toString());
+                tempVal = findViewById(R.id.txtNum2);
+                double num2 = tempVal.isEnabled() ? Double.parseDouble(tempVal.getText().toString()) : 0;
+                double respuesta = 0.0;
 
-                    if (opcion == 0) {
-                        spnDe = findViewById(R.id.spnDeMonedas);
-                        spnA = findViewById(R.id.spnAMonedas);
-                    } else if (opcion == 1) {
-                        spnDe = findViewById(R.id.spnDeLongitud);
-                        spnA = findViewById(R.id.spnALongitud);
-                    } else if (opcion == 2) {
-                        spnDe = findViewById(R.id.spnDeTiempo);
-                        spnA = findViewById(R.id.spnATiempo);
-                    } else if (opcion == 3) {
-                        spnDe = findViewById(R.id.spnDeAlmacenamiento);
-                        spnA = findViewById(R.id.spnAAlmacenamiento);
-                    } else if (opcion == 4) {
-                        spnDe = findViewById(R.id.spnDeTransferencia);
-                        spnA = findViewById(R.id.spnATransferencia);
-                    }
-                    else if (opcion == 5) {
-                        spnDe = findViewById(R.id.spnDeMasa);
-                        spnA = findViewById(R.id.spnAMasa);
-                    }
-                    else if (opcion == 6) {
-                        spnDe = findViewById(R.id.spnDeVolumen);
-                        spnA = findViewById(R.id.spnAVolumen);
-                    }
-
-                    int de = spnDe.getSelectedItemPosition();
-                    int a = spnA.getSelectedItemPosition();
-
-                    String cantidadTexto = textCantidad.getText().toString().trim();
-                    if (cantidadTexto.isEmpty()) {
-                        Toast.makeText(MainActivity.this, "⚠ Seleciona un numero", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-
-                    double cantidad = Double.parseDouble(cantidadTexto);
-
-                    if (cantidad < 0) {
-                        Toast.makeText(MainActivity.this, "⚠ Ingrese un número positivo", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-
-                    // Validar que no sean la misma unidad de origen y destino
-                    if (de == a) {
-                        Toast.makeText(MainActivity.this, "⚠ Seleccione diferentes unidades", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-
-                    // Realizar la conversión
-                    double respuesta = objConversores.convertir(opcion, de, a, cantidad);
-
-                    Toast.makeText(MainActivity.this, "✅ La Conversión es : " + respuesta, Toast.LENGTH_LONG).show();
-                    lblRespuesta.setText("Respuesta: " + respuesta);
-
-                } catch (NumberFormatException e) {
-                    Toast.makeText(MainActivity.this, "❌ Error: Solo se permiten números", Toast.LENGTH_SHORT).show();
-                } catch (Exception e) {
-                    Toast.makeText(MainActivity.this, "❌ Error inesperado: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                spn = findViewById(R.id.spnOpciones);
+                switch (spn.getSelectedItemPosition()){
+                    case 0:
+                        respuesta = num1 + num2;
+                        break;
+                    case 1:
+                        respuesta = num1 - num2;
+                        break;
+                    case 2:
+                        respuesta = num1 * num2;
+                        break;
+                    case 3:
+                        respuesta = num1 / num2;
+                        break;
+                    case  4:
+                        respuesta = Math.pow(num1, num2);
+                        break;
+                    case 5:
+                        respuesta = (num1 * num2) / 100;
+                        break;
+                    case 6:
+                        respuesta = Math.sqrt(num1);
+                        break;
+                    case  7:
+                        respuesta = 1;
+                        for (int i = 2; i <= num1; i++){
+                            respuesta *= i;
+                        }
+                        break;
+                    case 8:
+                        respuesta = Math.cbrt(num1);
+                        break;
+                    case 9:
+                        respuesta = num1 % num2;
+                        break;
+                    case 10:
+                        respuesta = Math.max(num1,num2);
+                        break;
                 }
+
+                tempVal = findViewById(R.id.lblRespuesta);
+                tempVal.setText("Respuesta: " + respuesta);
             }
         });
-    }
-    class conversores {
-        double[][] valores = {
-                {1, 0.98, 7.73, 25.45, 36.78, 508.87, 8.74, 0.85, 0.75, 150.30, 17.11},
-                {1, 0.001, 100, 1000, 39.37, 3.2808, 1.0936, 0.00062137, 0.00054, 0.00000062137},
-                {1, 1.0/60, 1.0/3600, 1.0/86400, 1.0/604800, 1.0/2628000, 1.0/31536000, 1.0/315360000, 1.0/3153600000.0, 1.0/31536000000.0},
-                {1, 0.000976563, 9.53674e-7, 9.31323e-10, 9.09495e-13, 8.88178e-16, 8.67362e-19, 8.47033e-22, 8.27181e-25, 8.07794e-28},
-                {1, 0.001, 0.000001, 0.000000001, 0.000000000001, 0.000000000000001, 0.000000000000000001, 0.000000000000000000001, 0.000000000000000000000001, 0.000000000000000000000000001},
-                {1, 0.001, 1000, 1000000, 35.274, 2.20462, 0.157473, 0.001, 0.000001, 0.00000110231},
-                {1, 1000, 0.001, 35.1951, 4.16667, 1.30795, 0.264172, 0.001, 0.000001, 0.00000110231},
-        };
-
-        public double convertir(int opcion, int de, int a, double cantidad){
-            return valores[opcion][a] / valores[opcion][de] * cantidad;
-        }
     }
 }
