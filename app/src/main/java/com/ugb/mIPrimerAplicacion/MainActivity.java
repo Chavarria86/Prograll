ae0ca73ac3e05431dcaaf2b1ff18dde2d51290b8
chavarria86 <USSS027724@ugb.edu.sv>
2025-02-26 22:19:13 -0600

    primer parcial

--- app/src/main/java/com/ugb/mIPrimerAplicacion/MainActivity.java
+++ app/src/main/java/com/ugb/mIPrimerAplicacion/MainActivity.java
@@ -2,7 +2,6 @@ package com.ugb.mIPrimerAplicacion;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -17,95 +16,73 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn;
+    TabHost tbh;
+    Button btnCalcular;
     TextView tempVal;
-    RadioGroup rgb;
-    RadioButton opt;
-    EditText num1, num2;
+    EditText textCantidad;
+    TextView lblRespuesta;
+    Spinner spnDe, spnA;
 
-    Spinner spn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btnCalcular);
-        num1 = findViewById(R.id.txtNum1);
-        num2 = findViewById(R.id.txtNum2);
+        tbh = findViewById(R.id.tbhConversor);
+        btnCalcular = findViewById(R.id.btnCalcular);
+        textCantidad = findViewById(R.id.txtNum1);
+        lblRespuesta = findViewById(R.id.lblRespuesta);
 
-        spn = findViewById(R.id.spnOpciones);
-        spn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        tbh.setup();
+        TabHost.TabSpec tab1 = tbh.newTabSpec("Moneda");
+        tab1.setIndicator("Moneda");
+        tab1.setContent(R.id.tabCuota);
+        tbh.addTab(tab1);
+
+        TabHost.TabSpec tab2 = tbh.newTabSpec("Longitud");
+        tab2.setIndicator("Longitud");
+        tab2.setContent(R.id.tabLongitud);
+        tbh.addTab(tab2);
+
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if (position == 6 || position == 7 || position == 8) {
-                    num2.setText("0.00");
-                    num2.setEnabled(false);
-                } else {
-                    num2.setEnabled(true);
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tempVal = findViewById(R.id.txtNum1);
-                double num1 = Double.parseDouble(tempVal.getText().toString());
-                tempVal = findViewById(R.id.txtNum2);
-                double num2 = tempVal.isEnabled() ? Double.parseDouble(tempVal.getText().toString()) : 0;
-                double respuesta = 0.0;
-
-                spn = findViewById(R.id.spnOpciones);
-                switch (spn.getSelectedItemPosition()){
-                    case 0:
-                        respuesta = num1 + num2;
-                        break;
-                    case 1:
-                        respuesta = num1 - num2;
-                        break;
-                    case 2:
-                        respuesta = num1 * num2;
-                        break;
-                    case 3:
-                        respuesta = num1 / num2;
-                        break;
-                    case  4:
-                        respuesta = Math.pow(num1, num2);
-                        break;
-                    case 5:
-                        respuesta = (num1 * num2) / 100;
-                        break;
-                    case 6:
-                        respuesta = Math.sqrt(num1);
-                        break;
-                    case  7:
-                        respuesta = 1;
-                        for (int i = 2; i <= num1; i++){
-                            respuesta *= i;
-                        }
-                        break;
-                    case 8:
-                        respuesta = Math.cbrt(num1);
-                        break;
-                    case 9:
-                        respuesta = num1 % num2;
-                        break;
-                    case 10:
-                        respuesta = Math.max(num1,num2);
-                        break;
-                }
-
-                tempVal = findViewById(R.id.lblRespuesta);
-                tempVal.setText("Respuesta: " + respuesta);
+            public void onClick(View view) {
+                calcularTarifa();
             }
         });
+
+    }
+    private void calcularTarifa() {
+        String input = textCantidad.getText().toString();
+        if (input.isEmpty()) {
+            lblRespuesta.setText("Ingrese la cantidad de metros consumidos");
+            return;
+        }
+
+        int metrosConsumidos = Integer.parseInt(input);
+        double totalPagar = 0;
+
+        if (metrosConsumidos <= 18) {
+            totalPagar = 6;
+        } else if (metrosConsumidos <= 28) {
+            totalPagar = 6 + (metrosConsumidos - 18) * 0.45;
+        } else {
+            totalPagar = 6 + (10 * 0.45) + ((metrosConsumidos - 28) * 0.65);
+        }
+
+        lblRespuesta.setText("Total a pagar: $" + String.format("%.2f", totalPagar));
     }
 }
