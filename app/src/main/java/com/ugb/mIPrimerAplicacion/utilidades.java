0eff6dbe7ecb7389f59ce8fa4a00576a3e149786
chavarria86 <USSS027724@ugb.edu.sv>
2025-04-22 04:09:44 -0600

    terminacion de la agenda amigossqlite

---
+++ app/src/main/java/com/ugb/mIPrimerAplicacion/utilidades.java
@@ -0,0 +1,14 @@
+package com.ugb.mIPrimerAplicacion;
+
+import java.util.Base64;
+
+public class utilidades {
+    static String url_consulta = "http://192.168.85.23:5984/agenda/_design/agenda/_view/agenda";
+    static String url_mto = "http://192.168.1.7:5984/agenda";
+    static String user = "Emer_progra";
+    static String passwd = "12345678";
+    static String credencialesCodificadas = Base64.getEncoder().encodeToString((user + ":" + passwd).getBytes());
+    public String generarUnicoId(){
+        return java.util.UUID.randomUUID().toString();
+    }
+}
