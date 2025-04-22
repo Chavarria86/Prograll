48f10744ec7822c0fa635bfa1d260944032b37ec
chavarria86 <USSS027724@ugb.edu.sv>
2025-04-22 04:47:30 -0600

    sensor de luz

--- app/src/main/java/com/ugb/mIPrimerAplicacion/MainActivity.java
+++ app/src/main/java/com/ugb/mIPrimerAplicacion/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ugb.mIPrimerAplicacion;
 
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -21,7 +22,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 
 public class MainActivity extends AppCompatActivity {
     TextView tempVal;
@@ -52,25 +52,25 @@ public class MainActivity extends AppCompatActivity {
         sensorManager.unregisterListener(sensorEventListener);
     }
     private void sensorLuz(){
-        tempVal = findViewById(R.id.lblSensorProximidad);
+        tempVal = findViewById(R.id.lblSensorLuz);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         if( sensor==null ){
-            tempVal.setText("Tu dispositivo, NO tiene el senor de PROXIMIDAD");
+            tempVal.setText("Tu dispositivo, NO tiene el senor de LUZ");
             finish();
         }
         sensorEventListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
                 double valor = event.values[0];
-                tempVal.setText("Proximidad: "+ valor);
+                tempVal.setText("Cantidad de Luz: "+ valor);
 
-                if(valor<=4){
-                    getWindow().getDecorView().setBackgroundColor(Color.BLACK);
-                }else if(valor<=8){
+                if(valor<=20){
                     getWindow().getDecorView().setBackgroundColor(Color.GRAY);
+                }else if(valor<=50){
+                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
                 }else{
-                    getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
                 }
             }
             @Override
